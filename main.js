27929e7c4097d5845f038ac0b68477cb66242e8c
Guilherme Bruzzi <guibruzzi@gmail.com>
2017-01-10 10:54:44 -0200

    Updating test according to better lib

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-import "@vtex/android-log-to-web"
+import LogAndroid from "@vtex/android-log-to-web"
 
+LogAndroid.v('hello Android', 'world')
 Log.v('hello', 'world')
+LogAndroid.d('hello Android', 'world')
 Log.d('hello', 'world')
+LogAndroid.i('hello Android', 'world')
 Log.i('hello', 'world')
+LogAndroid.w('hello Android', 'world')
 Log.w('hello', 'world')
+LogAndroid.e('hello Android', 'world')
 Log.e('hello', 'world')
